8ec4a89719c38de2e2f511da7da0a30b24b15a5f
Victor Costan <costan@gmail.com>
2011-01-04 13:22:19 -0500

    Fixed crash in cleanup.

--- lib/bcpm/cleanup.rb
+++ lib/bcpm/cleanup.rb
@@ -10,8 +10,9 @@ module Cleanup
     Bcpm::Player.list.each do |player|
       Bcpm::Player.uninstall player if /^bcpmtest/ =~ player
     end
-    
+        
     temp_path = File.join(Dir.tmpdir, 'bcpm')
+    return unless File.exist?(temp_path)
     Dir.entries(temp_path).each do |entry|
       next if ['.', '..'].include? entry
       path = File.join temp_path, entry
