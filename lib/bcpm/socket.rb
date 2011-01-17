7148f6e093d379ffdb052889e7b7005c66be5d28
Victor Costan <costan@gmail.com>
2011-01-17 18:57:11 -0500

    Bugfix in Socket.hostname.

--- lib/bcpm/socket.rb
+++ lib/bcpm/socket.rb
@@ -3,6 +3,6 @@ require 'socket'
 unless Socket.respond_to? :hostname
   # :nodoc: Monkey-patch Socket to add hostname method for old Rubies.
   def Socket.hostname
-    @__hostname ||= `hostname`
+    @__hostname ||= `hostname`.strip
   end
 end
